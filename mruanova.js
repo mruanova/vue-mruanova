63d29091e1b63544587236dda7c4087e3d7e5925
Mauricio Ruanova-Hurtado <mruanova@users.noreply.github.com>
2017-05-01 21:52:35 -0500

    Add files via upload
    updated google maps markers and mobile scroll animation

--- mruanova.js
+++ mruanova.js
@@ -1,5 +1,3 @@
-
-//READY
 $(document).ready(function () {
     //GET
     $.get("https://246gg84zg8.execute-api.us-west-2.amazonaws.com/prod/projects", function (response) {
@@ -9,7 +7,7 @@ $(document).ready(function () {
         });
         //DISPLAY PROJECTS
         for (var i = 0; i < response.Items.length; i++) {
-            var project = "<div class='project'>"
+            var project = "<div id='project"+response.Items[i].ProjectId+"' class='project'>"
                 + "<div class='company'>"+response.Items[i].Name+"</div>"
                 + "<div class='website'>"+response.Items[i].Website+"</div>"
                 + "<div class='position'>"+response.Items[i].Position+"</div>"
@@ -18,16 +16,44 @@ $(document).ready(function () {
             $("#projects").append(project);
         };
     }, "json");
-    $(".animation").css("top", "425px");
+    $("#project1").click();
+    $(".animation").css("top", "450px");
 });
 //map.setCenter
+var markers = [];
 $(document).on("click", ".project", function () {
+    var name = $(this)[0].children[0].innerText;
+    var website = $(this)[0].children[1].innerText;
+    var position = $(this)[0].children[2].innerText;
     var address = $(this)[0].children[3].innerText;
     geocoder.geocode({ 'address': address }, function (results, status) {
         if (status == 'OK') {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(null);
+            }
             map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                title: name
+            });
+            markers.push(marker);
+            var contentString = '<div id="content">' +
+                '<div class="link2">' + name + '</div>' +
+                '<div id="bodyContent">' +
+                '<div class="website">' + website + '</div>' +
+                '<div class="address">'+ address + '</div>' +
+                '</div>' +
+                '</div>';
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
         }
     });
+
 });
 // Get the modal
 var modal = document.getElementById('myModal');
@@ -55,139 +81,4 @@ function initialize() {
         center: latlng
     };
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    //1
-    var title1 = "Coding DOJO";
-    var address1 = "213 W Institute Pl #610, Chicago, IL 60610";
-    var website1 = "www.codingdojo.com";
-    geocoder.geocode({ 'address': address1 }, function (results, status) {
-        if (status == 'OK') {
-            //map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                title: title1
-            });
-            var contentString = '<div id="content">' +
-                '<div class="link2">' + title1 + '</div>' +
-                '<div id="bodyContent">' +
-                '<div class="website">' + website1 + '</div>' +
-                '<div class="address">'+ address1 + '</div>' +
-                '</div>' +
-                '</div>';
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
-        }
-    });
-    //2
-    var title2 = "CSG International";
-    var address2 = "33 W Monroe St #900, Chicago, IL 60603";
-    var website2 = "www.csgi.com";
-    geocoder.geocode({ 'address': address2 }, function (results, status) {
-        if (status == 'OK') {
-            //map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                title: title2
-            });
-            var contentString = '<div id="content">' +
-                '<div class="link2">' + title2 + '</div>' +
-                '<div id="bodyContent">' +
-                '<div class="website">' + website2 + '</div>' +
-                '<div class="address">' + address2 + '</div>' +
-                '</div>' +
-                '</div>';
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
-        }
-    });
-    //3
-    var title3 = "AON Consulting Inc";
-    var address3 = "200 E Randolph St #7, Chicago, IL 60601";
-    var website3 = "www.aon.com";
-    geocoder.geocode({ 'address': address3 }, function (results, status) {
-        if (status == 'OK') {
-            //map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                title: title3
-            });
-            var contentString = '<div id="content">' +
-                '<div class="link2">' + title3 + '</div>' +
-                '<div id="bodyContent">' +
-                '<div class="website">' + website3 + '</div>' +
-                '<div class="address">' + address3 + '</div>' +
-                '</div>' +
-                '</div>';
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
-        }
-    });
-    //4
-    var title4 = "AVANADE Inc";
-    var address4 = "161 N Clark St #3700, Chicago, IL 60601";
-    var website4 = "www.avanade.com";
-    geocoder.geocode({ 'address': address4 }, function (results, status) {
-        if (status == 'OK') {
-            //map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                title: title4
-            });
-            var contentString = '<div id="content">' +
-                '<div class="link2">' + title4 + '</div>' +
-                '<div id="bodyContent">' +
-                '<div class="website">' + website4 + '</div>' +
-                '<div class="address">' + address4 + '</div>' +
-                '</div>' +
-                '</div>';
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
-        }
-    });
-    //5
-    var title5 = "G/O Digital Marketing";
-    var address5 = "225 N Michigan Ave #1600, Chicago, IL 60601";
-    var website5 = "www.godigitalmarketing.com";
-    geocoder.geocode({ 'address': address5 }, function (results, status) {
-        if (status == 'OK') {
-            //map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                title: title5
-            });
-            var contentString = '<div id="content">' +
-                '<div class="link2">' + title5 + '</div>' +
-                '<div id="bodyContent">' +
-                '<div class="website">' + website5 + '</div>' +
-                '<div class="address">' + address5 + '</div>' +
-                '</div>' +
-                '</div>';
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-            marker.addListener('click', function () {
-                infowindow.open(map, marker);
-            });
-        }
-    });
 };
