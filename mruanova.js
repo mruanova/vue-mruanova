dddd7197c3c880f6ec32b921efc8f6c24e0afe20
Mauricio Ruanova-Hurtado <mruanova@users.noreply.github.com>
2017-05-07 11:35:39 -0500

    Add files via upload
    update files

--- mruanova.js
+++ mruanova.js
@@ -61,33 +61,7 @@ function addMarker(index,title,website,address) {
 
 $(document).ready(function () {
     //console.log(api);
-    /*
-    $.get(api, function (response) {
-        response.Items.sort(function (a, b) {
-            return parseFloat(a.ProjectId) - parseFloat(b.ProjectId);
-        });
-        for (var i = 0; i < response.Items.length; i++) {
-            if(i<5){
-                var project = "<div id='project"+response.Items[i].ProjectId+"' class='project'>"
-                    + "<div class='company'>"+response.Items[i].Name+"</div>"
-                    + "<div class='website'>"+response.Items[i].Website+"</div>"
-                    + "<div class='position'>"+response.Items[i].Position+"</div>"
-                    + "<div class='address'>"+response.Items[i].Address+"</div>"
-                    + "</div>";
-                // console.log(response.Items[i].ProjectId);
-                // console.log(response.Items[i].Name);
-                // console.log(response.Items[i].Website);
-                // console.log(response.Items[i].Position);
-                // console.log(response.Items[i].Address);
-                $("#projects").append(project);
-                addMarker(i,response.Items[i].Name,response.Items[i].Website,response.Items[i].Address);
-            }
-        };
-    }, "json");
     //$(".animation").css("top", "450px");
-    */
-
-    /* backbone and underscore */
 
     var ProjectModel = Backbone.Model.extend({
         defaults: {
@@ -139,7 +113,6 @@ $(document).ready(function () {
             'click':'mapGeoCode'
         },
         mapGeoCode: function(){             
-            //["0"].model.attributes.Name
             var name = $(this)[0].model.attributes.Name;
             var website = $(this)[0].model.attributes.Website;
             var position = $(this)[0].model.attributes.Position;
